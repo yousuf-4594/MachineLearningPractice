08da18d8fde870e8452b8fd5a84075eec54e9bae
yousuf-4594 <cysiddiqui@gmail.com>
2023-11-04 20:53:28 +0500

    Updated project dependencies to address security vulnerabilities.

--- bootloader.cpp
+++ bootloader.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdlib>
-#include <windows.h> // Include the Windows API header
 
 int main() {
     // Specify the full path to the Python executable
@@ -13,9 +12,6 @@ int main() {
     } else {
         std::cout << "Error executing the Python script." << std::endl;
     }
-
-    // Introduce a delay (e.g., 5000 milliseconds) before waiting for a key press
-    Sleep(5000);
 
     // Wait for a key press before closing the window
     std::cin.get();
