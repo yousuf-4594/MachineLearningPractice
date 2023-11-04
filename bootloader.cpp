eef1914b5d754f4a39290fa38e1c2368ff8359c0
yousuf-4594 <cysiddiqui@gmail.com>
2023-11-04 20:39:52 +0500

    Improved model interpretability with SHAP values.

---
+++ bootloader.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    // Specify the full path to the Python executable
+    const char* command = "C:\\Python311\\python.exe GeometricAnalysis.py";
+
+    int returnCode = std::system(command);
+
+    if (returnCode == 0) {
+        std::cout << "Python script executed successfully." << std::endl;
+    } else {
+        std::cout << "Error executing the Python script." << std::endl;
+    }
+
+    return 0;
+}
+
